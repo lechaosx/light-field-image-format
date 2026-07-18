dc633c8718d67b9c6c09cca6522b36f910e90323
Drahomír Dlabaja <lechaosx@gmail.com>
2026-07-18 13:13:47 +0200

    c++20/23: std::byteswap and std::endian in endian.h

--- liblfif/include/components/endian.h
+++ liblfif/include/components/endian.h
@@ -8,60 +8,28 @@
 
 #pragma once
 
-#include <algorithm>
+#include <bit>
+#include <concepts>
 #include <istream>
 #include <ostream>
 
-
-/**
-* @brief  Function which performs endian swap.
-* @param  data The data to be swapped.
-* @return Swapped data.
-*/
-template <typename T>
-inline T endianSwap(T data) {
-  uint8_t *ptr = reinterpret_cast<uint8_t *>(&data);
-  std::reverse(ptr, ptr + sizeof(T));
-  return data;
-}
-
-/**
-* @brief  Function which performs swap only if local endian != big endian.
-* @param  data The data to be swapped.
-* @return Swapped data.
-*/
-template <typename T>
+template <std::integral T>
 inline T bigEndianSwap(T data) {
-  return (htobe16(1) == 1) ? data : endianSwap(data);
+  return std::endian::native == std::endian::big ? data : std::byteswap(data);
 }
 
-/**
-* @brief  Function which performs swap only if local endian != little endian.
-* @param  data The data to be swapped.
-* @return Swapped data.
-*/
-template <typename T>
+template <std::integral T>
 inline T littleEndianSwap(T data) {
-  return (htobe16(1) == 1) ? endianSwap(data) : data;
+  return std::endian::native == std::endian::little ? data : std::byteswap(data);
 }
 
-/**
-* @brief Function which writes value to stream in big endian.
-* @param data The data to be written.
-* @param stream The stream to which the data will be written.
-*/
-template<typename T>
+template<std::integral T>
 inline void writeValueToStream(T data, std::ostream &stream) {
   T dataBE = bigEndianSwap(data);
   stream.write(reinterpret_cast<const char *>(&dataBE), sizeof(dataBE));
 }
 
-/**
-* @brief  Function which reads big endian value from stream.
-* @param  stream The stream from which the data will be read.
-* @return The data to be read.
-*/
-template<typename T>
+template<std::integral T>
 inline T readValueFromStream(std::istream &stream) {
   T dataBE {};
   stream.read(reinterpret_cast<char *>(&dataBE), sizeof(dataBE));
