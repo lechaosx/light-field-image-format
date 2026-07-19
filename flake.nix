2e21118c9b370797003176856a2194baed39a300
Drahomír Dlabaja <lechaosx@gmail.com>
2026-07-19 13:12:53 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,15 +3,18 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    devShells.${system}.default = (pkgs.mkShell.override { stdenv = pkgs.gcc16Stdenv; }) {
+    devShells.${system}.default = (pkgs.mkShell.override {stdenv = pkgs.gcc16Stdenv;}) {
       nativeBuildInputs = [
         pkgs.cmake
         pkgs.ninja
-        pkgs.clang-tools_18
+        pkgs.llvmPackages_22.clang-tools
         pkgs.gdb
         pkgs.doxygen
       ];
