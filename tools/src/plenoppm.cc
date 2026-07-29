d5603769575635101d344369c11fcb54e43f0d2c
Drahomír Dlabaja <lechaosx@gmail.com>
2026-07-29 16:25:02 +0200

    fix: bound masked PPM discovery to filesystem entries

--- tools/src/plenoppm.cc
+++ tools/src/plenoppm.cc
@@ -4,20 +4,52 @@
 \******************************************************************************/
 
 #include "plenoppm.h"
-#include "file_mask.h"
 
+#include <algorithm>
+#include <glob.h>
+#include <memory>
 #include <stdexcept>
+#include <string>
 #include <system_error>
 #include <utility>
 
 std::vector<PPM> mapPPMs(std::string_view input_file_mask) {
-  const FileMask file_name {input_file_mask};
-  const size_t file_name_count = file_name.count();
+  std::string pattern;
+  bool is_mask {};
+  for (const char character : input_file_mask) {
+    if (character == '#') {
+      pattern += "[0-9]";
+      is_mask = true;
+    } else {
+      if (character == '\\' || character == '*' || character == '?'
+          || character == '[' || character == ']') {
+        pattern += '\\';
+      }
+      pattern += character;
+    }
+  }
+
+  std::vector<std::string> file_names;
+  if (is_mask) {
+    glob_t matches {};
+    std::unique_ptr<glob_t, decltype(&globfree)> release_matches {
+        &matches, &globfree};
+    const int status = glob(pattern.c_str(), 0, nullptr, &matches);
+    if (status != 0 && status != GLOB_NOMATCH) {
+      throw std::runtime_error("cannot expand PPM file mask");
+    }
+    for (size_t i = 0; i < matches.gl_pathc; ++i) {
+      file_names.emplace_back(matches.gl_pathv[i]);
+    }
+  } else {
+    file_names.emplace_back(input_file_mask);
+  }
+  std::ranges::sort(file_names);
 
   std::vector<PPM> images;
-  for (size_t image = 0; image < file_name_count; ++image) {
+  for (const std::string &file_name : file_names) {
     try {
-      PPM ppm = PPM::map(file_name[image]);
+      PPM ppm = PPM::map(file_name);
       if (!images.empty()
           && (ppm.width() != images.front().width()
               || ppm.height() != images.front().height()
